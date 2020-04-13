7aef9f24f5a594577b2b8b8dc49deb0362593442
IronHeart7334 <greengrappler12@gmail.com>
2020-04-13 12:17:49 -0700

    Yeah, printFile works. Next, parse it tomarrow if I have time

--- interpolate.py
+++ interpolate.py
@@ -5,6 +5,12 @@ def getCmdLineArgs():
     parser.add_argument("filename", metavar="filename", type=argparse.FileType("r"), nargs=1, help="the csv file to interpolate")
     args = parser.parse_args()
     return args
+
+def printFile(file):
+    for line in file:
+        print(line)
     
 if __name__ == "__main__":
-    print(getCmdLineArgs().filename)
+    file = getCmdLineArgs().filename[0]
+    print(file.name)
+    printFile(file)
