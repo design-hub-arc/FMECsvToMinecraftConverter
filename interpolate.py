3cbb44eee400f1f40a0a2cf0ced8a5173a96a38c
IronHeart7334 <greengrappler12@gmail.com>
2020-04-14 11:03:55 -0700

    Still a work in progress, but I've gotta take a break

--- interpolate.py
+++ interpolate.py
@@ -21,19 +21,43 @@ def printFile(file):
         print(line)
 
 def interpolate(inputFilePath, outputFilePath):
-    inF = open(inputFilePath, "r")
-    outF = open(outputFilePath, "w")
+    inF = open(inputFilePath, mode="r", encoding="utf-8-sig")
+    outF = open(outputFilePath, mode="w")
+    containsRGB = True
+    error = False
     try:
-        firstLine = inF.readline()
-        print(firstLine)
-    except Error as e:
+        firstLine = inF.readline().strip()
+        #print(firstLine)
+        containsRGB = "r" in firstLine.lower()
+    except Exception as e:
         print("Error while reading file: " + str(e))
+
+    if not error:
+        # oh wait, I do need to cache lines to compute partial derivatives across multiple axes
+        fLines = []
+        minX = None
+        minY = None
+        minZ = None
+        for line in inF:
+            if "x" in line:
+                continue # skip first line if it hasn't been read yet
+            line = line.strip().replace(",", " ") # FME output doesn't contain commas, so make sure all data stays that way
+            if not containsRGB:
+                line = line + " 0 0 0"
+            fLines.append(line)
+            # next, need to split lines, and update minimums (integers)
+            print(line)
+        # shift everything to (0,0,0) being the lowest coordinate. Use 2D array for O(1) lookup
+        # interpolate, write to output file (don't forget "x y z r g b"!)
     inF.close()
     outF.close()
 
 if __name__ == "__main__":
     args = getCmdLineArgs()
     sfile = args.sourcefile[0]
-    print(os.path.abspath(sfile.name))
+    spath = os.path.abspath(sfile.name)
+    print(spath)
     rfile = args.resultfile[0]
-    print(os.path.abspath(rfile.name))
+    rpath = os.path.abspath(rfile.name)
+    print(rpath)
+    interpolate(spath, rpath)
