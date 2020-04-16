752ef35a398d49c0993cf61df22cbfb775653e41
IronHeart7334 <greengrappler12@gmail.com>
2020-04-16 12:25:57 -0700

    python not able to find module in subdirectory, I'll figure it out tomarrow, because I'm out of time for today

--- scripts/getClosestBlock.py
+++ scripts/getClosestBlock.py
@@ -1,6 +1,75 @@
+import math
+
 '''
 This file is used by FME to find which
 blockID and blockData to assign to each
 point in a point cloud, based on their
 RGB values.
 '''
+
+# change this line if you move the colorToBlockTable.csv file
+BLOCK_TABLE_PATH = "./colorToBlockTable.csv"
+
+class Block:
+    def __init__(self, r, g, b, id, data):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.id = id
+        self.data = data
+
+    def __str__(self):
+        return "Block #{0}/{1} ({2}, {3}, {4})".format(self.id, self.data, self.r, self.g, self.b)
+
+def extractBlockHeaders(headerArray):
+    required = ["r", "g", "b", "blockID", "blockData"]
+    headerToCol = {}
+    for header in required:
+        if header in headerArray:
+            headerToCol[header] = headerArray.index(header)
+        else:
+            raise Exception("Header array is missing the required header '{0}', instead, it contains the headers [{1}].".format(header, ", ".join(headerArray)));
+    return headerToCol
+
+def getBlockList():
+    filePath = BLOCK_TABLE_PATH
+    blockList = []
+    file = None
+    try:
+        file = open(filePath, "r")
+        headers = file.readline().strip().split(",")
+        headerCols = extractBlockHeaders(headers)
+        for line in file:
+            line = line.strip().split(",") # the block table file uses proper CSV format, unlike FME
+            blockList.append(Block(
+                int(line[headerCols["r"]]),
+                int(line[headerCols["g"]]),
+                int(line[headerCols["b"]]),
+                int(line[headerCols["blockID"]]),
+                int(line[headerCols["blockData"]])
+            ))
+    except Exception as e:
+        print("Failed to read " + filePath)
+        print(e)
+
+    if file is not None:
+        file.close()
+
+    return blockList
+
+def dist(point1, point2):
+    return math.sqrt(
+        math.pow(point1[0] - point2[0], 2)
+        + math.pow(point1[1] - point2[1], 2)
+        + math.pow(point1[2] - point2[2], 2)
+    )
+
+def closestBlockColor(rgb, blockList):
+    shortestDist = None
+    closestBlock = None
+    for block in blockList:
+        distance = dist(rgb, (block.r, block.g, block.b))
+        if shortestDist is None or distance < shortestDist:
+            shortestDist = distance
+            closestBlock = block
+    return closestBlock  
