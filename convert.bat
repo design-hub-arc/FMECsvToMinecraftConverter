df4d3f4b19411602440e84581fe523a5ddfbedaa
IronHeart7334 <greengrappler12@gmail.com>
2020-04-21 08:58:59 -0700

    OK, looks like Randy doesn't want color after all. Will add a 'white out' python script so I can leave in support for color later

---
+++ convert.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Converting %~f1
+echo this isn't implemented yet
+
+REM this line is a comment, or REMark
+REM "C:\Program Files\FME\fme.exe" "C:\Users\Matt\Documents\FME Projects\Converter\Converter.fmw" --DestDataset_MINECRAFT "C:\Users\Matt\AppData\Roaming\.minecraft\saves" --worldName "temp" --pointReduction "100" --SourceDataset_CSV2 "C:\Users\Matt\Documents\FME Projects\Converter\convertedData\aerospaceRvtMaybeColor.csv" --FEATURE_TYPES ""
