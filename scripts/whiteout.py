9bd67ec6fed4c963dd0449f3fcaecb0d697f7210
IronHeart7334 <greengrappler12@gmail.com>
2020-04-21 09:41:39 -0700

    looks like whiteout works. Still need to add white block to block table though

--- scripts/whiteout.py
+++ scripts/whiteout.py
@@ -22,6 +22,22 @@ def getCmdLineArgs():
     }
     return args
 
+def whiteout(srcPath, resultPath):
+    with open(srcPath, "r") as source:
+        with open(resultPath, "w") as result:
+            result.write(source.readline().replace(",", " ")) #add headers
+            for line in source:
+                #                            need to replace these two individually, otherwise we may get two spaces in a row
+                #                            TODO: replace with regex so it splits on any number of spaces
+                split = line.strip().replace(", ", " ").replace(",", " ").split(" ") # FME exports CSV with spaces instead of commas sometimes
+                print(split)
+                split[3] = "255"
+                split[4] = "255"
+                split[5] = "255"
+                newLine = " ".join(split)
+                result.write(newLine + "\n")
+
 if __name__ == "__main__":
     args = getCmdLineArgs()
     print(args)
+    whiteout(args["sourceFilePath"], args["resultFilePath"])
