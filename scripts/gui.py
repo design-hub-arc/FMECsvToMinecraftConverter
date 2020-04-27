c90d1ad5500cc92298d978c5f6358b97f7411727
IronHeart7334 <greengrappler12@gmail.com>
2020-04-27 09:57:37 -0700

    There, got the button to work

--- scripts/gui.py
+++ scripts/gui.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk # "Themed widgets". Whatever that means
 
+
+
 # Following the example at https://tkdocs.com/tutorial
 
 def launch():
@@ -9,26 +11,40 @@ def launch():
         I don't know how it
         will be formatted
     """
+
+    def convert(*args):
+        print(args)
+        # needs to be nested to access input
+        print("Input is " + input.get())
+
     root = Tk() # what does this do? Is it like a JFrame?
     root.title("Minecraft Converter")
     # Create the content pane
-    content = ttk.Frame(root, padding="1 1 1 1") # not sure what padding does
+    #                   root is the parent
+    content = ttk.Frame(root, padding="10 10 10 10")
     content.grid(column=0, row=0, sticky=(N, W, E, S)) # What does this do? Does it make the content "stick" to 0,0?
     root.columnconfigure(0, weight=1) # fill available space in the window
     root.rowconfigure(0, weight=1)
 
     # Add info text
-    text = ttk.Label(content, text=desc).grid(column=1, row=1, sticky=(N, W))
+    text = ttk.Label(content, text=desc).grid(column=1, row=1, sticky=N)
 
     # Add input to the GUI
-    input = StringVar()
-    inputGui = ttk.Entry(content, width=5, textvariable=input)
-    inputGui.grid(column=1, row=2, sticky=(N, W))
+    input = StringVar() #         using textvariable allows this to auto-update when input changes
+    inputGui = ttk.Entry(content, textvariable=input)
+    inputGui.grid(column=1, row=2, sticky=N)
 
     # Add button
-    button = ttk.Button(content, text="Convert")
-    button.grid(column=1, row=3, sticky=(N, W))
+    button = ttk.Button(content, text="Convert", command=convert)
+    button.grid(column=1, row=3, sticky=N)
 
+    # Output
+    output = StringVar()
+    outputPane = ttk.Label(content, textvariable=output)
+    outputPane.grid(column=1, row=4, sticky=N)
+
+    inputGui.focus()
+    root.bind("<Return>", convert) # Press enter to convert
     root.mainloop()
 
 if __name__ == "__main__":
