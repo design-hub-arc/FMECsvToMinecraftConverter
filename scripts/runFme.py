a65c1da968574bd3138ee5d1c3cfa3c94ba3a818
IronHeart7334 <greengrappler12@gmail.com>
2020-05-04 09:48:10 -0700

    Looks like it works with CSV files containing no headers and no color, but the data I was given was way too sparse to produce a good world.

--- scripts/runFme.py
+++ scripts/runFme.py
@@ -130,9 +130,25 @@ def runCsvRepair(sourceDataset: str, resultFileName=None, outputListener=print)-
 def repairCsv(inPath: str, outPath: str):
     with open(inPath, mode="rt") as inFile:
         with open(outPath, mode="wt") as outFile:
+            headers = [header.strip().lower() for header in inFile.readline().strip().split(",")]
+            hasColor = False
+            if not ("x" in headers and "y" in headers and "z" in headers):
+                # no headers present, so we must write them ourselves.
+                outFile.write("x,y,z,r,g,b\n")
+                # but, this means the first line is data, so don't forget to write it.
+                if len(headers) == 3:
+                    hasColor = False
+                    headers.extend(["255", "255", "255"])
+                else:
+                    hasColor = True
+                outFile.write(",".join(headers) + "\n")
+            # more here
+
             for line in inFile:
                 line = line.strip()
-                print(line)
+                if not hasColor:
+                    #        don't forget the comma at the start
+                    line += ",255,255,255"
                 outFile.write(line + "\n")
 
 
